4237b215884ed5153d608ade18481fd74f3536cd
makhammadjonov <118533833+makhammadjonov@users.noreply.github.com>
2022-11-28 19:32:22 +0500

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+       number = 1111;
+       System.out.println(number+number+number+number); 
     }
 }
